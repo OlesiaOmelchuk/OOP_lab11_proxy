5d6f74aa9a98ff387f0997290f2ceee9d0937179
OlesiaOmelchuk <olesyao2003@gmail.com>
2023-01-05 14:58:50 +0200

    added proxyImage

---
+++ src/main/java/ua/edu/ucu/task_3/Main.java
@@ -0,0 +1,8 @@
+package ua.edu.ucu.task_3;
+
+public class Main {
+    public static void main(String[] args) {
+        ProxyImage proxyImage = new ProxyImage("autumn.jpg");
+        proxyImage.display();
+    }
+}
---
+++ src/main/java/ua/edu/ucu/task_3/ProxyImage.java
@@ -0,0 +1,18 @@
+package ua.edu.ucu.task_3;
+
+public class ProxyImage implements MyImage {
+    private String filename;
+    private RealImage realImage;
+
+    public ProxyImage(String filename) {
+        this.filename = filename;
+    }
+
+    @Override
+    public void display() {
+        if (realImage == null) {
+            realImage = new RealImage(filename);
+        }
+        realImage.display();
+    }
+}
